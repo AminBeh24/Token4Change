cfb703f93904c58d60fcad6e069a9afa45f11a66
AminBeh24 <am.beh6722@gmail.com>
2025-02-11 16:53:02 +0100

    the end of day one
    converted php to html

--- Index.php
+++ Index.php
@@ -7,4 +7,15 @@
     <title>Token</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <h1>Hello world</h1>
+
+<?php
+
+echo 'Hello world!';
+
+$val1 = 5;
+$val2 = 10;
+$result = $val1 + $val2;
+
+?>
